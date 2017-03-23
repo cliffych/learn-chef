550a633ffc4ab60e0f357fced780f9e8fa16f03c
Yun Chin Hsieh <hsiehy1@hsiehy1-y5g3qn.wifi.gene.com>
2017-03-22 17:21:09 -0700

    Add generated delivery build cookbook

--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -1,6 +1,6 @@
 #
-# Cookbook Name:: build_cookbook
+# Cookbook:: build_cookbook
 # Recipe:: unit
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright:: 2017, The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::unit'
